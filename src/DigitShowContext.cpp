9830e3212c5214167126fbe34f2b02302cf717af
Makoto KUNO <mkt-kuno@iis.u-tokyo.ac.jp>
2026-08-22 14:52:01 +0900

    Add DigitShowContext singleton (skeleton, not yet wired)

---
+++ src/DigitShowContext.cpp
@@ -0,0 +1,264 @@
+/*
+ * DigitShowBasicTS - Hollow Torsional Shear Triaxial Test Control Software
+ * Copyright (C) 2026 Makoto KUNO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.h"
+#include "DigitShowContext.h"
+
+// Singleton instance
+static DigitShowContext g_Context;
+static bool g_ContextInitialized = false;
+
+DigitShowContext* GetContext()
+{
+	if (!g_ContextInitialized) {
+		InitContext(&g_Context);
+		g_ContextInitialized = true;
+	}
+	return &g_Context;
+}
+
+void InitContext(DigitShowContext* ctx)
+{
+	if (ctx == NULL) return;
+
+	int	i, j;
+
+	// CAIO return codes / error strings
+	ctx->Ret = 0;
+	ctx->Ret2 = 0;
+	memset(ctx->ErrorString, 0, sizeof(ctx->ErrorString));
+	ctx->TextString = _T("");
+
+	// Boards (zero all POD fields)
+	for (i = 0; i < AD_MAX_BOARDS; i++) {
+		memset(&ctx->ad[i], 0, sizeof(ctx->ad[i]));
+	}
+	for (i = 0; i < DA_MAX_BOARDS; i++) {
+		memset(&ctx->da[i], 0, sizeof(ctx->da[i]));
+	}
+	ctx->NumAD = 1;		// The Number of A/D Board ( NUMAD=0-2 )
+	ctx->NumDA = 1;		// The Number of D/A Board ( NUMDA=0-1 )
+
+	// Raw transfer buffers
+	memset(ctx->DaData, 0, sizeof(ctx->DaData));
+	memset(ctx->AdData0, 0, sizeof(ctx->AdData0));
+	memset(ctx->AdData1, 0, sizeof(ctx->AdData1));
+	for (i = 0; i < AD_MAX_BOARDS; i++) {
+		ctx->pSmplData[i] = NULL;
+		ctx->hHeap[i] = NULL;
+	}
+
+	// Acquisition state
+	ctx->AdMaxCH = 0;
+	ctx->AdEvent = 0;
+	ctx->SavingClock = 0.0f;
+	ctx->SavingTime = 300;
+	ctx->TotalSamplingTimes = 0;
+	ctx->CurrentSamplingTimes = 0;
+	ctx->AllocatedMemory = 0.0f;
+	ctx->AvSmplNum = 10;
+
+	// Analog input measurement data
+	for (i = 0; i < AI_MAX_CHANNELS; i++) {
+		ctx->ai.raw[i] = 0.0f;
+		ctx->ai.phy[i] = 0.0;
+		ctx->ai.param[i] = 0.0;
+		ctx->ai.cal.a[i] = 0.0;
+		ctx->ai.cal.b[i] = 1.0;
+		ctx->ai.cal.c[i] = 0.0;
+	}
+
+	// Analog output setpoints
+	for (i = 0; i < AO_MAX_CHANNELS; i++) {
+		ctx->ao.raw[i] = 0.0f;
+		ctx->ao.cal.a[i] = 0.0;
+		ctx->ao.cal.b[i] = 0.0;
+	}
+
+	// Channel names
+//  @note Hashimoto modified 2022.2.28
+	ctx->NameV[0]=_T("V.Load");		ctx->NameP[0]=_T("V.Load,N");
+	ctx->NameV[1]=_T("T.Load ");	ctx->NameP[1]=_T("Torque,Ncm");
+	ctx->NameV[2]=_T("POT1");		ctx->NameP[2]=_T("POT1,rad");
+	ctx->NameV[3]=_T("POT2");		ctx->NameP[3]=_T("POT2,rad");
+	ctx->NameV[4]=_T("HCDPT");		ctx->NameP[4]=_T("EffectiveStress,kPa");
+	ctx->NameV[5]=_T("ExtLVDT");	ctx->NameP[5]=_T("V.DispEXT,mm");
+	ctx->NameV[6]=_T("LDT1");	    ctx->NameP[6]=_T("LDT1,mm");
+	ctx->NameV[7]=_T("CH7");		ctx->NameP[7]=_T("CH7");
+	ctx->NameV[8]=_T("CG1");		ctx->NameP[8]=_T("CG1,mm");
+	ctx->NameV[9]=_T("CG2");		ctx->NameP[9]=_T("CG2,mm");
+	ctx->NameV[10]=_T("CG3");		ctx->NameP[10]=_T("CG3,mm");
+	ctx->NameV[11]=_T("LDT2");		ctx->NameP[11]=_T("LDT2,mm");
+	ctx->NameV[12]=_T("CH12");		ctx->NameP[12]=_T("CH12");
+	ctx->NameV[13]=_T("LCDPT");		ctx->NameP[13]=_T("DeltaVol.,mm3");
+	ctx->NameV[14]=_T("CH14");		ctx->NameP[14]=_T("CH14");
+	ctx->NameV[15]=_T("CH15");		ctx->NameP[15]=_T("CH15");
+	for (i = 16; i < AI_MAX_CHANNELS; i++) {
+		CString tmp;
+		tmp.Format("CH%d", i);
+		ctx->NameV[i]=tmp;
+		ctx->NameP[i]=tmp;
+	}
+	ctx->NameDV[0]=_T("CH00: EP cell pressure");
+	ctx->NameDV[1]=_T("CH01: Axial Motor");
+	ctx->NameDV[2]=_T("CH02: Axial Clutch");
+	ctx->NameDV[3]=_T("CH03: Axial Motor Speed");
+	ctx->NameDV[4]=_T("CH04: Torsional Motor");
+	ctx->NameDV[5]=_T("CH05: Torsional Clutch");
+	ctx->NameDV[6]=_T("CH06: Torsional Motor Speed");
+
+	// Physical values
+	memset(&ctx->phys, 0, sizeof(ctx->phys));
+
+	// Specimen data
+	for (j = 0; j < 4; j++) {
+		ctx->specimen.DiameterIn[j]=60.0;
+		ctx->specimen.DiameterOut[j]=100.0;
+		ctx->specimen.Height[j]=150.0;
+		ctx->specimen.Volume[j]=3.141592*(100.0*100.0-60.0*60.0)/4*150.0;
+		ctx->specimen.DiaInMembrane[j]=60.0;
+		ctx->specimen.DiaOutMembrane[j]=100.0;
+		ctx->specimen.HeightInMembrane[j]=150.0;
+		ctx->specimen.HeightOutMembrane[j]=150.0;
+	}
+	ctx->specimen.MembraneModulus=1400.0;
+	ctx->specimen.MembraneThickness=0.3;
+	//ctx->specimen.RDiaInM=60.0;
+	//ctx->specimen.RDiaOutM=100.0;
+	ctx->specimen.RDiaInM = 59.85; //@note Hashimoto fixed 2022.12.22
+	ctx->specimen.RDiaOutM = 100.15; //@note Hashimoto fixed 2022.12.22
+	ctx->specimen.RHeightInM=150.0; //@note Hashimoto fixed 2022.12.22
+	ctx->specimen.RHeightOutM=150.0; //@note Hashimoto fixed 2022.12.22
+	ctx->specimen.RodArea=0.0;
+	ctx->specimen.CapWeight=0.0;
+
+	// Control target data
+	for (i = 0; i < CONTROL_MAX; i++) {
+		for (j = 0; j < 3; j++) {
+			ctx->control[i].flag[j]=FALSE;
+			ctx->control[i].time[j]=0;
+			ctx->control[i].p[j]=0.0;
+			ctx->control[i].q[j]=0.0;
+			ctx->control[i].u[j]=0.0;
+			ctx->control[i].sigma[j]=0.0;
+			ctx->control[i].sigmaRate[j]=0.0;
+			ctx->control[i].sigmaAmp[j]=0.0;
+			ctx->control[i].strain[j]=0.0;
+			ctx->control[i].strainRate[j]=0.0;
+			ctx->control[i].strainAmp[j]=0.0;
+		}
+		ctx->control[i].K0=1.0;
+		ctx->control[i].AxisClutch=0.0;
+		ctx->control[i].AxisSpeed=0.0;
+		ctx->control[i].TorsionClutch=0.0;
+		ctx->control[i].TorsionSpeed=0.0;
+	}
+	ctx->control[1].q[0]=1.0;
+	ctx->control[1].AxisSpeed = 100.0;
+
+	// D/A channel index assignments
+	ctx->daCh.EP_Cell         = DA_CH_EP_CELL;        // D/A Channel of EP (Cell Pressure)
+	ctx->daCh.AxisMotor       = DA_CH_AXIS_MOTOR;     // 0V:On, 5V:Off
+	ctx->daCh.AxisClutch      = DA_CH_AXIS_CLUTCH;    //
+	ctx->daCh.AxisSpeed       = DA_CH_AXIS_SPEED;     //
+	ctx->daCh.TorsionMotor    = DA_CH_TORSION_MOTOR;  // 0V:On, 5V:Off
+	ctx->daCh.TorsionClutch   = DA_CH_TORSION_CLUTCH; //
+	ctx->daCh.TorsionSpeed    = DA_CH_TORSION_SPEED;  //
+
+	// Motor drive voltages
+	ctx->volt.Down = 0.0f;
+	ctx->volt.Up   = 5.0f;
+	ctx->volt.CW   = 0.0f;
+	ctx->volt.CCW  = 5.0f;
+
+	// D/A calibration factors
+	ctx->ao.cal.a[DA_CH_AXIS_SPEED]=0.0033333;     // Axial Motor Speed (V/RPM)
+	ctx->ao.cal.b[DA_CH_AXIS_SPEED]=0.0;
+	ctx->ao.cal.a[DA_CH_TORSION_SPEED]=0.0034483;  // Torsion Motor Speed (V/RPM)
+	ctx->ao.cal.b[DA_CH_TORSION_SPEED]=0.0;
+	ctx->ao.cal.a[DA_CH_EP_CELL]=0.0175;           // EP of Cell Pressure (V/kPa)
+	ctx->ao.cal.b[DA_CH_EP_CELL]=0.0;
+
+	// Control state
+	ctx->ControlID = 0;
+	ctx->NumCyclic = 0;
+	ctx->NumSmallCyclic = 0;
+	ctx->StepTime = 0.0;
+	ctx->StepDisplay = 0;                          // 2021.12.07 Edited by M.Kuno
+
+	// Control targets
+	ctx->target.sz = 0.0;
+	ctx->target.sr = 0.0;
+	ctx->target.tzq = 0.0;
+	ctx->target.ez = 0.0;
+	ctx->target.gzq = 0.0;
+
+	// Error tolerances
+	ctx->err.StressMotor= 0.5;                     // (kPa)
+	ctx->err.StressAir  = 0.5;                     // (kPa)
+	ctx->err.StrainEz   = 0.0005;                  // (---)
+	ctx->err.StrainGzq  = 0.0005;                  // (---)
+
+	// System flags
+	ctx->flags.SetBoard = FALSE;
+	ctx->flags.SaveData = FALSE;
+	ctx->flags.FIFO = FALSE;
+	ctx->flags.Cyclic = FALSE;
+	ctx->flags.SetRs232c = FALSE;
+	ctx->flags.SetBalance = FALSE;
+	ctx->flags.COM[0] = FALSE;
+	ctx->flags.COM[1] = FALSE;
+
+	// Time management
+	ctx->timeSettings.Interval1 = 200;             // Time interval (ms) to display output data.
+	ctx->timeSettings.Interval2 = 500;             // Time interval (ms) to control.
+	ctx->timeSettings.Interval3 = 1000;            // Time interval (ms) to save the data.
+	ctx->SequentTime1 = 0;
+	ctx->SequentTime2 = 0.0;
+	ctx->CtrlStepTime = 0.0;
+
+	// Data file handles
+	ctx->FileSaveData0 = NULL;
+	ctx->FileSaveData1 = NULL;
+	ctx->FileSaveData2 = NULL;
+
+	// RS232C (electronic balance)
+	ctx->rs232c.hComm[0] = NULL;
+	ctx->rs232c.hComm[1] = NULL;
+	memset(&ctx->rs232c.CommDCB[0], 0, sizeof(DCB));
+	memset(&ctx->rs232c.CommDCB[1], 0, sizeof(DCB));
+	ctx->rs232c.CommName[0]=_T("COM1");
+	ctx->rs232c.CommName[1]=_T("COM2");
+	memset(&ctx->rs232c.Comstat1, 0, sizeof(COMSTAT));
+	memset(&ctx->rs232c.Comstat2, 0, sizeof(COMSTAT));
+	ctx->rs232c.ReadBuffer1 = _T("");
+	ctx->rs232c.ReadBuffer2 = _T("");
+	ctx->rs232c.WriteBuffer1 = _T("");
+	ctx->rs232c.WriteBuffer2 = _T("");
+	ctx->rs232c.TmpBuffer = _T("");
+	ctx->rs232c.hEvent = NULL;
+
+	// Control file data
+	ctx->controlFile.CurrentNum = 0;
+	for (i = 0; i < CONTROLFILE_STEPS; i++) {
+		ctx->controlFile.Num[i] = 0;
+		for (j = 0; j < CONTROLFILE_PARAS; j++) {
+			ctx->controlFile.Para[i][j] = 0.0;
+		}
+	}
+}
---
+++ src/DigitShowContext.h
@@ -0,0 +1,342 @@
+/*
+ * DigitShowBasicTS - Hollow Torsional Shear Triaxial Test Control Software
+ * Copyright (C) 2026 Makoto KUNO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __DIGITSHOWCONTEXT_H_INCLUDE__
+#define __DIGITSHOWCONTEXT_H_INCLUDE__
+
+#pragma once
+
+#include <afxwin.h>
+#include <stdio.h>
+
+// ── Array sizes ──────────────────────────────────────────
+#define AI_MAX_CHANNELS     32  // AI channels (primary board Ch00-15 + second board Ch16-31)
+#define AO_MAX_CHANNELS     16  // AO channels
+#define AD_MAX_BOARDS        2  // Number of A/D boards (NUMAD = 0-2)
+#define DA_MAX_BOARDS        1  // Number of D/A boards (NUMDA = 0-1)
+#define CONTROL_MAX         16  // Number of control target data sets
+#define CONTROLFILE_STEPS  256  // Number of steps in a control file
+#define CONTROLFILE_PARAS   16  // Number of parameters per control file step
+#define NAME_DV_MAX          8  // Number of D/A channel names
+
+// Raw transfer buffer size [samples] (64 MiB / board, same as legacy globals)
+#define AD_BUFFER_SIZE 16777216L
+#define DA_BUFFER_SIZE     262144L
+
+// ── D/A channel index assignments (fixed hardware wiring) ──
+#define DA_CH_EP_CELL         0  // EP cell pressure
+#define DA_CH_AXIS_MOTOR      1  // Axial motor On/Off (0V = On, 5V = Off)
+#define DA_CH_AXIS_CLUTCH     2  // Axial clutch
+#define DA_CH_AXIS_SPEED      3  // Axial motor speed
+#define DA_CH_TORSION_MOTOR   4  // Torsion motor On/Off (0V = On, 5V = Off)
+#define DA_CH_TORSION_CLUTCH  5  // Torsion clutch
+#define DA_CH_TORSION_SPEED   6  // Torsion motor speed
+
+/**
+ * CAIO board configuration (CONTEC AIO-WDM)
+ */
+struct AdBoardConfig {
+	short  Id;              // ID returned by AioInit
+	short  Channels;        // number of enabled channels
+	short  Range;           // range code
+	float  RangeMax;        // maximum voltage of the range
+	float  RangeMin;        // minimum voltage of the range
+	short  Resolution;      // resolution [bit]
+	short  InputMethod;     // single-ended / differential
+	short  MemoryType;      // FIFO / RING
+	float  ScanClock;       // scan clock [us/ch]
+	float  SamplingClock;   // sampling clock [us]
+	long   SamplingTimes;   // sampling times per event
+};
+
+struct DaBoardConfig {
+	short  Id;
+	short  Channels;
+	short  Range;
+	float  RangeMax;
+	float  RangeMin;
+	short  Resolution;
+};
+
+/**
+ * Specimen data (hollow cylinder)
+ */
+struct SpecimenData {
+	double DiameterIn[4];       double DiameterOut[4];
+	double Height[4];
+	double Volume[4];
+	double MembraneModulus;     double MembraneThickness;
+	double RodArea;             double CapWeight;
+	double RDiaInM;             double RDiaOutM;
+	double RHeightInM;          double RHeightOutM;
+	double DiaInMembrane[4];    double DiaOutMembrane[4];
+	double HeightInMembrane[4]; double HeightOutMembrane[4];
+};
+
+/**
+ * Control target data per control ID
+ */
+struct ControlData {
+	bool   flag[3];
+	int    time[3];
+	double p[3];
+	double q[3];
+	double u[3];
+	double sigma[3];
+	double sigmaRate[3];
+	double sigmaAmp[3];
+	double strain[3];
+	double strainRate[3];
+	double strainAmp[3];
+	double K0;
+	double AxisClutch;
+	double AxisSpeed;
+	double TorsionClutch;
+	double TorsionSpeed;
+};
+
+/**
+ * Physical values computed by Cal_Physical() / Cal_Param()
+ */
+struct PhysicalValues {
+	double rotation1;           // POT1 angle [rad]
+	double rotation2;           // POT2 angle [rad]
+	double BW1;                 // balance weight 1
+	double BW2;                 // balance weight 2
+	double height;              // current specimen height
+	double area;                // current specimen area
+	double volume;              // current specimen volume
+	double diameter_in;         // current inner diameter
+	double diameter_out;        // current outer diameter
+	double diameterInM;         // current inner membrane diameter
+	double diameterOutM;        // current outer membrane diameter
+	double heightInM;           // current inner membrane height
+	double heightOutM;          // current outer membrane height
+	double cell_in;             // inner cell pressure
+	double cell_out;            // outer cell pressure
+	double sz;                  // axial stress
+	double sr;                  // radial stress
+	double sq;                  // circumferential stress
+	double szq;                 // shear stress
+	double p;                   // mean stress
+	double q;                   // deviatoric stress
+	double ez;                  // axial strain
+	double er;                  // radial strain
+	double eq;                  // circumferential strain
+	double gzq1;                // shear strain 1
+	double gzq2;                // shear strain 2
+	double ev;                  // volumetric strain
+	double ezInM;               // inner membrane axial strain
+	double ezOutM;              // outer membrane axial strain
+	double eqInM;               // inner membrane circumferential strain
+	double eqOutM;              // outer membrane circumferential strain
+	double gzqInM;              // inner membrane shear strain
+	double gzqOutM;             // outer membrane shear strain
+	double PressureInM;         // inner membrane pressure
+	double PressureOutM;        // outer membrane pressure
+	double ForceM;              // membrane axial force
+	double TorqueM;             // membrane resistance torque
+};
+
+/**
+ * Control file data (File Controllable Consolidation)
+ */
+struct ControlFileData {
+	int    CurrentNum;
+	int    Num[CONTROLFILE_STEPS];
+	double Para[CONTROLFILE_STEPS][CONTROLFILE_PARAS];
+};
+
+/**
+ * Timer intervals [ms]
+ */
+struct TimeSettings {
+	unsigned int Interval1;  // Timer 1: acquisition / display
+	unsigned int Interval2;  // Timer 2: control feedback
+	unsigned int Interval3;  // Timer 3: data save
+};
+
+/**
+ * System flags
+ */
+struct SystemFlags {
+	bool SetBoard;    // board initialization accomplished
+	bool SaveData;    // saving data to file
+	bool FIFO;        // FIFO acquisition running
+	bool Cyclic;      // cyclic loading running
+	bool SetRs232c;   // RS232C port opened
+	bool SetBalance;  // electronic balance connected
+	bool COM[2];      // COM port status
+};
+
+/**
+ * Main application context structure.
+ * Single global instance replaces the former scattered extern variables.
+ */
+struct DigitShowContext {
+	// CAIO return codes / error strings
+	long   Ret;
+	long   Ret2;
+	char   ErrorString[256];
+	CString TextString;
+
+	// Boards
+	int            NumAD;                    // number of A/D boards (0-2)
+	int            NumDA;                    // number of D/A boards (0-1)
+	AdBoardConfig  ad[AD_MAX_BOARDS];
+	DaBoardConfig  da[DA_MAX_BOARDS];
+
+	// Raw transfer buffers
+	long   DaData[DA_BUFFER_SIZE];
+	long   AdData0[AD_BUFFER_SIZE];
+	long   AdData1[AD_BUFFER_SIZE];
+	PVOID  pSmplData[AD_MAX_BOARDS];         // save buffer source
+	HANDLE hHeap[AD_MAX_BOARDS];
+
+	// Acquisition state
+	int   AdMaxCH;
+	long  AdEvent;
+	float SavingClock;                       // [us] current saving clock
+	int   SavingTime;                        // [s] total saving time
+	long  TotalSamplingTimes;
+	long  CurrentSamplingTimes;
+	float AllocatedMemory;                   // [MiB]
+	int   AvSmplNum;                         // number of samples averaged for display
+
+	// Channel names
+	CString NameV[AI_MAX_CHANNELS];          // voltage channel names
+	CString NameP[AI_MAX_CHANNELS];          // physical channel names
+	CString NameDV[NAME_DV_MAX];             // D/A channel names
+
+	// Analog input measurement data
+	struct {
+		float  raw[AI_MAX_CHANNELS];         // averaged ADC voltages [V]
+		double phy[AI_MAX_CHANNELS];         // calibrated physical values
+		double param[AI_MAX_CHANNELS];       // derived parameters for display
+		struct {
+			double a[AI_MAX_CHANNELS];       // quadratic coefficient
+			double b[AI_MAX_CHANNELS];       // linear coefficient
+			double c[AI_MAX_CHANNELS];       // offset
+		} cal;
+	} ai;
+
+	// Analog output setpoints
+	struct {
+		float  raw[AO_MAX_CHANNELS];         // output voltages [V]
+		struct {
+			double a[AO_MAX_CHANNELS];       // gain
+			double b[AO_MAX_CHANNELS];       // offset
+		} cal;
+	} ao;
+
+	// Physical values
+	PhysicalValues phys;
+
+	// Specimen and control
+	SpecimenData  specimen;
+	ControlData   control[CONTROL_MAX];
+	ControlFileData controlFile;
+
+	// D/A channel index assignments
+	struct {
+		int EP_Cell;
+		int AxisMotor;
+		int AxisClutch;
+		int AxisSpeed;
+		int TorsionMotor;
+		int TorsionClutch;
+		int TorsionSpeed;
+	} daCh;
+
+	// Motor drive voltages
+	struct {
+		float Up;                            // on voltage
+		float Down;                          // off voltage
+		float CW;                            // clockwise voltage
+		float CCW;                           // counter-clockwise voltage
+	} volt;
+
+	// Control state
+	int    ControlID;
+	int    NumCyclic;
+	int    NumSmallCyclic;
+	double StepTime;
+	int    StepDisplay;
+
+	// Control targets
+	struct {
+		double sz;
+		double sr;
+		double tzq;
+		double ez;
+		double gzq;
+	} target;
+
+	// Error tolerances
+	struct {
+		double StressMotor;                  // stress control sensitivity (motor) [kPa]
+		double StressAir;                    // stress control sensitivity (air) [kPa]
+		double StrainEz;                     // axial strain sensitivity
+		double StrainGzq;                    // shear strain sensitivity
+	} err;
+
+	// System flags
+	SystemFlags flags;
+
+	// Time management
+	TimeSettings timeSettings;
+	CTime        StartTime;
+	CTime        NowTime;
+	CTimeSpan    SpanTime;
+	CString      SNowTime;
+	long         SequentTime1;
+	double       SequentTime2;
+	double       CtrlStepTime;
+
+	// Data file handles
+	FILE* FileSaveData0;                     // voltage value log
+	FILE* FileSaveData1;                     // physical value log
+	FILE* FileSaveData2;                     // parameter value log
+
+	// RS232C (electronic balance)
+	struct {
+		HANDLE   hComm[2];
+		DCB      CommDCB[2];
+		CString  CommName[2];
+		COMSTAT  Comstat1;
+		COMSTAT  Comstat2;
+		CString  ReadBuffer1;
+		CString  ReadBuffer2;
+		CString  WriteBuffer1;
+		CString  WriteBuffer2;
+		CString  TmpBuffer;
+		HANDLE   hEvent;
+	} rs232c;
+};
+
+/**
+ * Get the global context instance (singleton)
+ */
+DigitShowContext* GetContext();
+
+/**
+ * Initialize the context with default values
+ */
+void InitContext(DigitShowContext* ctx);
+
+#endif // __DIGITSHOWCONTEXT_H_INCLUDE__
